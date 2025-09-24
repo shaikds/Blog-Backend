6de898a66434ab40c9edc5d0e93487b57f2c6cb3
shaikds <shai6661@gmail.com>
2025-09-24 10:38:57 +0300

    process of modularization of main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import datetime
+import os
 import time
 import uuid
 from typing import List
-
-import supabase
+from supabase import create_client, Client
 from fastapi import *
 
 from app.models.Post import PostCreate, Post
+from app.services.supabase_service import SupabaseService
 
 app = FastAPI()
 posts = {}
+supabaseService = SupabaseService()
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.time()
@@ -26,10 +28,13 @@ async def create_post(post: PostCreate):
     post_json["created_at"] =  datetime.datetime.now()
     post_json["updated_at"] = None
 
-    postModel = Post(**post_json)
-    posts[postModel.id] = postModel
 
-    return postModel
+    # post creation
+    postModel = Post(**post_json)
+
+    await supabase.create_post(postModel)
+
+    return imagesLinkList
 
 @app.put("/posts/{id}/", status_code = 200)
 async def update_post(id: uuid.UUID, title: str, content: str, images: List[File(...)]):
@@ -40,8 +45,9 @@ async def update_post(id: uuid.UUID, title: str, content: str, images: List[File
     post_model.images = images
     post_model.updated_at = datetime.datetime.now() ## INITs updated_at field
 
-    db_storage = supabase.Client.storage
-    supabase = supabase.Client(db_storage)
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
 
 
 
